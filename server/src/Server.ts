83ffa0ee860c4b42a92bb5d669d9c46d3bae12bb
Lester Tay <lester.tay@accenture.com>
2020-10-11 19:01:27 +0800

    to heroku

--- server/src/Server.ts
+++ server/src/Server.ts
@@ -1,10 +1,10 @@
-import express, { Request, Response } from "express";
-import cors from "cors";
+var express = require("express");
+var cors = require("cors");
 const app = express();
 
 app.use(cors());
 
-app.get("/", (req: Request, res: Response) => {
+app.get("/", (req, res) => {
   res.status(200).send("Hello World12345!");
 });
 

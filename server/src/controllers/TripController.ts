e3c21dd2f010e703486d9c7abd1651b086218ef7
hanliong-looi <hanliong99@gmail.com>
2020-10-27 22:21:38 +0800

    hi lester

--- server/src/controllers/TripController.ts
+++ server/src/controllers/TripController.ts
@@ -3,6 +3,7 @@ import {io} from '../Server';
 import Trip from './../database/Trip';
 
 var commuterList: string[] = [];
+var busRoute: string[] = ['Hall 10', 'North Hill', 'Hall 11', 'Tamarind', 'Hall 13', 'Hall 14', 'North Spine', 'South Spine', 'Hall 7']
 
 // - GET - /alltrip # returns all trips
 export let allTrip = async (socket: any) => {
@@ -85,84 +86,107 @@ export let updateTrip = (socket: any, data: any) => {
 
 }
 
-export let findRegex = async (req: Request, res: Response) => {
-
-}
-
 // - GET - /findCovid # finds that covid fucker
 
 export let findCovidFucker = async (req: Request, res: Response) => {
-	
-	let startList: Array<string> = req.body.startTime.split(' ');
-	let stopList: Array<string> = req.body.stopTime.split(' ');
 
-	let startTimeList: Array<string> = startList[4].split(':');
-	let stopTimeList: Array<string> = stopList[4].split(':');
+	Trip.findById(req.body._id, (err: any, covidTrip: any) => {
+		if(err)
+			res.send(err);
+		else{
+			let startList: Array<string> = covidTrip.startTime.split(' ');
+			let stopList: Array<string> = covidTrip.stopTime.split(' ');
 
-	let startTimeNumber: number = parseInt(startTimeList[0]) * 60 * 60 + parseInt(startTimeList[1]) * 60 + parseInt(startTimeList[2]);
-	let stopTimeNumber: number = parseInt(stopTimeList[0]) * 60 * 60 + parseInt(stopTimeList[1]) * 60 + parseInt(stopTimeList[2]);
+			let startTimeList: Array<string> = startList[4].split(':');
+			let stopTimeList: Array<string> = stopList[4].split(':');
 
-	if(startList[5].localeCompare('pm') == 0){
-		startTimeNumber += 12 * 60 *60;
-	}
+			let startTimeNumber: number = parseInt(startTimeList[0]) * 60 * 60 + parseInt(startTimeList[1]) * 60 + parseInt(startTimeList[2]);
+			let stopTimeNumber: number = parseInt(stopTimeList[0]) * 60 * 60 + parseInt(stopTimeList[1]) * 60 + parseInt(stopTimeList[2]);
 
-	if(stopList[5].localeCompare('pm') == 0){
-		stopTimeNumber += 12 * 60 * 60;
-	}
-
-	const filter = {
-		busDriver: req.body.busDriver,
-		busPlate: req.body.busPlate,
-		startTime: new RegExp(`${startList[1]} ${startList[2]} ${startList[3]}`)
-		
-	};
-
-	Trip.find(filter, (err: any, passengers: any) => {
-		if (!err){
-			console.log('found,', passengers)
-			for(let i = 0; i < passengers.length; i ++){
-				let pStartList: Array<string> = passengers[i].startTime.split(' ');
-				let pStopList: Array<string> = passengers[i].stopTime.split(' ');
-
-				console.log('pstartlist ', pStartList)
-				console.log('pstoplist', pStopList)
-
-				let pStartTimeList: Array<string> = pStartList[4].split(':');
-				let pStopTimeList: Array<string> = pStopList[4].split(':');
-
-				console.log('pstarttimelist ', pStartTimeList)
-				console.log('pstoptimelist', pStopTimeList)
-
-				let pStartTimeNumber: number = parseInt(pStartTimeList[0]) * 60 * 60 + parseInt(pStartTimeList[1]) * 60 + parseInt(pStartTimeList[2]);
-				let pStopTimeNumber: number = parseInt(pStopTimeList[0]) * 60 * 60 + parseInt(pStopTimeList[1]) * 60 + parseInt(pStopTimeList[2]);
-			
-
-				console.log(pStartList[5].localeCompare('pm'))
-				console.log(pStopList[5].localeCompare('pm'))
-
-				if(pStartList[5].localeCompare('pm') == 0){
-					pStartTimeNumber = pStopTimeNumber + 12 * 60 *60;
-				}
-			
-				if(pStopList[5] == 'pm'){
-					pStopTimeNumber = pStopTimeNumber + 12*60*60;
-				}
-
-				console.log('pstarttimenumber ', pStartTimeNumber)
-				console.log('pstoptimenumber', pStopTimeNumber)
-
-				if(pStartTimeNumber > stopTimeNumber || pStopTimeNumber < startTimeNumber){
-					passengers.remove(passengers[i]);
-					i--;
-				}
+			if(startList[5].localeCompare('pm') == 0){
+				startTimeNumber += 12 * 60 *60;
 			}
 
-			res.send(passengers);
-		}
-	}).then(data=>data).catch(err=>err);
-	
-	
+			if(stopList[5].localeCompare('pm') == 0){
+				stopTimeNumber += 12 * 60 * 60;
+			}
 
+			const filter = {
+				busDriver: covidTrip.busDriver,
+				busPlate: covidTrip.busPlate,
+				startTime: new RegExp(`${startList[1]} ${startList[2]} ${startList[3]}`)
+				
+			};
+
+			Trip.find(filter, (err: any, passengers: any) => {
+				if (!err){
+					console.log('found,', passengers)
+					for(let i = 0; i < passengers.length; i ++){
+						let pStartList: Array<string> = passengers[i].startTime.split(' ');
+						let pStopList: Array<string> = passengers[i].stopTime.split(' ');
+
+						console.log('pstartlist ', pStartList)
+						console.log('pstoplist', pStopList)
+
+						let pStartTimeList: Array<string> = pStartList[4].split(':');
+						let pStopTimeList: Array<string> = pStopList[4].split(':');
+
+						console.log('pstarttimelist ', pStartTimeList)
+						console.log('pstoptimelist', pStopTimeList)
+
+						let pStartTimeNumber: number = parseInt(pStartTimeList[0]) * 60 * 60 + parseInt(pStartTimeList[1]) * 60 + parseInt(pStartTimeList[2]);
+						let pStopTimeNumber: number = parseInt(pStopTimeList[0]) * 60 * 60 + parseInt(pStopTimeList[1]) * 60 + parseInt(pStopTimeList[2]);
+					
+
+						console.log(pStartList[5].localeCompare('pm'))
+						console.log(pStopList[5].localeCompare('pm'))
+
+						if(pStartList[5].localeCompare('pm') == 0){
+							pStartTimeNumber = pStopTimeNumber + 12 * 60 * 60;
+						}
+					
+						if(pStopList[5] == 'pm'){
+							pStopTimeNumber = pStopTimeNumber + 12 * 60 * 60;
+						}
+
+						console.log('pstarttimenumber ', pStartTimeNumber)
+						console.log('pstoptimenumber', pStopTimeNumber)
+
+						if(pStartTimeNumber > stopTimeNumber || pStopTimeNumber < startTimeNumber){
+							passengers.remove(passengers[i]);
+							i--;
+						}
+					}
+
+					var returnRes: string[][] = [[], [], [], [], [], [], [], [], []];
+					var startIndex: number = 0;
+					var stopIndex: number = 0;
+
+					for(let i = 0; i < passengers.length; i++){
+						for(let j = 0; j < busRoute.length; j++){
+							if(busRoute[j] == passengers[i].startLoc){
+								startIndex = j;
+								break;
+							}
+						}
+						for(let j = 0; j < busRoute.length; j++){
+							if(busRoute[j] == passengers[i].stopLoc){
+								stopIndex = j;
+								break;
+							}
+						}
+						if(stopIndex < startIndex){
+							stopIndex += 9;
+						}
+						for(let j = startIndex; j <= stopIndex; j++){
+							returnRes[j%9].push(passengers[i].commuterName)
+						}
+					}
+					res.send(returnRes);
+				}
+			}).then(data=>data).catch(err=>err);
+		}
+	})
 	// var index = commuterList.indexOf(data.commuterName);
   	// commuterList.splice(index, 1);
 	// socket.emit('COMMUTER_COUNT_UPDATE', {data: commuterList})
